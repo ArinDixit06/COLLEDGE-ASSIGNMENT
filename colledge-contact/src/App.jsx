3dfb3b768d35c8a40cf3c9212ccd8300283ccdfd
Arin  Dixit <id2013663@gmail.com>
2026-01-03 13:47:09 +0530

    Update App.jsx

--- colledge-contact/src/App.jsx
+++ colledge-contact/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import ContactForm from "./ContactForm";
+import ContactForm from "./components/ContactForm";
 
 const API = import.meta.env.VITE_API_URL;
 
